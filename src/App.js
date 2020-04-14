c80b8ae2c6ca5a4584d6b00dd97128f1c14d6797
tagine <rana@ranafayez.com>
2020-04-14 02:09:59 -0400

    salary sorting feature and button added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,45 @@ import './App.css';
 
 import data from "./data/employeedata.json";
 
-function App() {
-  return (
-    <div className="page-container">
-      <header className="App-header">
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: data,
+      direction: {
+        salary: "asc"
+      }
+    }
+
+    this.sortBy = this.sortBy.bind(this)
+  }
+
+  sortBy(key) {
+    console.log(key)
+    console.log(typeof parseFloat)
+    
+    this.setState({
+      data: data.sort((a, b) => {
+      return parseFloat(a[key]) - parseFloat(b[key])
+    }) 
+    })
+  }
+
+  render() {
+    return (
+      <div className="page-container">
         <img src={logo} className="App-logo" alt="logo" />
-        <EmployeeTable data={data}></EmployeeTable>
-      </header>
-    </div>
-  );
+        <EmployeeTable
+          data={this.state.data}
+          sortBy={this.sortBy}
+        />
+      </div>
+    );
+
+  }
+
+
+
 }
 
 export default App;
--- src/components/employee-table/index.jsx
+++ src/components/employee-table/index.jsx
@@ -7,8 +7,13 @@ export default function EmployeeTable(props) {
             <thead>
                 <tr>
                     <th>Index</th>
-                    <th>Balance</th>
-                    <th>Name</th>
+                    <th><button
+                            onClick={() => props.sortBy("salary")}
+                        >Salary</button></th>
+                    <th>
+                        First Name
+                    </th>
+                    <th>Last Name</th>
                     <th>Company</th>
                     <th>Email</th>
                     <th>Phone</th>
@@ -20,6 +25,13 @@ export default function EmployeeTable(props) {
                     props.data.map(row => (
                         <tr>
                             <td>{row.index}</td>
+                            <td>{row.salary}</td>
+                            <td>{row.name.first}</td>
+                            <td>{row.name.last}</td>
+                            <td>{row.company}</td>
+                            <td>{row.email}</td>
+                            <td>{row.phone}</td>
+                            <td>{row.address}</td>
                         </tr>
                     ))
                 }
