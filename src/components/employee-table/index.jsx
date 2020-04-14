62dd8829ac420d77d8373722e742e7d6eb8ebb53
tagine <rana@ranafayez.com>
2020-04-14 02:38:07 -0400

    table converted to semantic UI styling and is now responsive

--- src/components/employee-table/index.jsx
+++ src/components/employee-table/index.jsx
@@ -1,41 +1,42 @@
 import React from "react";
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
 
 export default function EmployeeTable(props) {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Index</th>
-                    <th><button
-                            onClick={() => props.sortBy("salary")}
-                        >Salary</button></th>
-                    <th>
+        <Table celled striped>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Index</Table.HeaderCell>
+                    <Table.HeaderCell><button
+                            onClick={() => props.sortBy("salary")} className="ui small green button"
+                        >Salary</button></Table.HeaderCell>
+                    <Table.HeaderCell>
                         First Name
-                    </th>
-                    <th>Last Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                </tr>
-            </thead>
-            <tbody>
+                    </Table.HeaderCell>
+                    <Table.HeaderCell>Last Name</Table.HeaderCell>
+                    <Table.HeaderCell>Company</Table.HeaderCell>
+                    <Table.HeaderCell>Email</Table.HeaderCell>
+                    <Table.HeaderCell>Phone</Table.HeaderCell>
+                    <Table.HeaderCell>Address</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
                 {
                     props.data.map(row => (
-                        <tr>
-                            <td>{row.index}</td>
-                            <td>{row.salary}</td>
-                            <td>{row.name.first}</td>
-                            <td>{row.name.last}</td>
-                            <td>{row.company}</td>
-                            <td>{row.email}</td>
-                            <td>{row.phone}</td>
-                            <td>{row.address}</td>
-                        </tr>
+                        <Table.Row>
+                            <Table.Cell>{row.index}</Table.Cell>
+                            <Table.Cell>{row.salary}</Table.Cell>
+                            <Table.Cell>{row.name.first}</Table.Cell>
+                            <Table.Cell>{row.name.last}</Table.Cell>
+                            <Table.Cell>{row.company}</Table.Cell>
+                            <Table.Cell>{row.email}</Table.Cell>
+                            <Table.Cell>{row.phone}</Table.Cell>
+                            <Table.Cell>{row.address}</Table.Cell>
+                        </Table.Row>
                     ))
                 }
-            </tbody>
-        </table>
+            </Table.Body>
+        </Table>
     )
 }
